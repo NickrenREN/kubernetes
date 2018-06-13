9f72520c6801eeb7403e2b5bd2b9ba4d8105c0a0
NickrenREN <renyuquan@caicloud.io>
2018-06-13 10:27:13 +0800

    Update node status to store ERs

--- pkg/apis/core/types.go
+++ pkg/apis/core/types.go
@@ -3507,6 +3507,19 @@ type NodeStatus struct {
 	// List of volumes that are attached to the node.
 	// +optional
 	VolumesAttached []AttachedVolume
+
+	// List of ExtendedResources that are allocatable on this node
+	// values are names of ExtendedResources
+	// +optional
+	ExtendedResourceAllocatable []string
+	// List of all ExtendedResources on this node
+	// values are names of ExtendedResources
+	// +optional
+	ExtendedResourceCapacity []string
+	// List of ExtendedResources that were on this node, but removed now
+	// values are names of ExtendedResources
+	// +optional
+	ExtendedResourceRemoved []string
 }
 
 type UniqueVolumeName string
